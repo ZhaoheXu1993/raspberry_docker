c83f21a232cea3712671301848f407d0bf220198
ZhaoheXu1993 <zhaohexu1993@gmail.com>
2016-05-19 19:06:26 +0800

    rasp #3
    via requests.get

--- RaspBerry.py
+++ RaspBerry.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import requests
+import threading
+import time
 
 app = Flask(__name__)
 
@@ -6,6 +9,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
+
+def led_controller():
+    while True:
+        requests.get('http://9d542a1b.ngrok.io/led/switch/open')
+        time.sleep(10)
+        requests.get('http://9d542a1b.ngrok.io/led/switch/close')
+        time.sleep(10)
+
+
+t = threading.Thread(target=led_controller())
+t.start()
 
 
 if __name__ == '__main__':
