840ab2949af879a6e64a7d1ac08f6bc757449246
ZhaoheXu1993 <zhaohexu1993@gmail.com>
2016-05-19 21:08:45 +0800

    rasp #6
    add temperature&humidity

--- RaspBerry.py
+++ RaspBerry.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',
+                           temperature='',
+                           humidity='')
 
 
 def led_controller():
