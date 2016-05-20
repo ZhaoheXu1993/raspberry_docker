f8eb6f510df206637be84c0b5a6cd845928c5ee5
ZhaoheXu1993 <zhaohexu1993@gmail.com>
2016-05-20 14:01:35 +0800

    rasp #12
    temp & humity

--- RaspBerry.py
+++ RaspBerry.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import controller.led_controller as led_controller
 import threading
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
     t = threading.Thread(target=led_controller.LEDController().led_blink())
     t.start()
+    temperature = request.form.__getattribute__('temperature')
+    humidity = request.form.__getattribute__('humidity')
     return render_template('home.html',
-                           temperature='',
-                           humidity='')
+                           temperature=temperature,
+                           humidity=humidity)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
