853e265983c306c0a2f4c192bc5b4cc1fc70a6cc
ZhaoheXu1993 <zhaohexu1993@gmail.com>
2016-05-20 15:25:00 +0800

    rasp #15
    temp

--- RaspBerry.py
+++ RaspBerry.py
@@ -18,6 +18,7 @@ def hello_world():
                            temperature=temperature,
                            humidity=humidity)
 
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
